55160dfc127b9f87e4b473c49631220f9d14cb51
JuSaLine <82720577+JuSaLine@users.noreply.github.com>
2021-05-18 15:44:05 +0200

    Create script_02.js
    original

---
+++ script_02.js
@@ -0,0 +1,80 @@
+/***** Variablen 01 *******/
+
+// Zeilen-Kommentar
+
+/**
+    Block-Kommentar
+ */
+
+// Ausgabe
+//console.log("Hallo Welt!");
+//console.log(firstName);
+
+// Deklaration + Wertzuweisung I 
+/*
+let firstName; // Deklaration (Definition)
+firstName = "Max"; // Wertzuweisung
+console.log(firstName); // Ausgabe
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log(familyName); // Ausgabe
+console.log(firstName + " " + familyName);
+*/
+
+// Deklaration + Wertzuweisung II
+/*
+let firstName,familyName;
+firstName = prompt("Bitte Vornamen eingeben:");
+familyName = prompt("Bitte Namen eingeben:");
+console.log("Hallo, " + firstName + " " + familyName + "!");
+*/
+
+// JS ist eine untypisierte Sprache! | untyped
+/*
+let test; //Deklaration
+test = "hi"; //string
+test = 2; // number
+test = true; // boolean
+console.log("Typ: " + typeof test);
+console.log("Inhalt: " + test);
+*/
+
+/***** Variablen 02 *******/
+/*
+// Deklaration 
+let ageJohn, ageMark;
+let birthYearJohn,birthYearMark; 
+// Wertzuweisung | Assignment
+let date = new Date();
+let year = date.getFullYear();
+ageJohn = 25;
+ageMark = 30;
+//Operation (Differenz)
+birthYearJohn = year - ageJohn;
+birthYearMark = year - ageMark;
+console.log("Aktuelles Jahr: " +  year);
+console.log("birthYearJohn: " + birthYearJohn);
+console.log("birthYearMark: " + birthYearMark);
+*/
+
+/***** Variablen 03 *******/
+/*
+let ageJohn, ageMark;
+ageJohn = 25;
+ageMark = 30;
+// Operation (Vergleich/Test)
+let isJohnOlder = (ageJohn > ageMark);
+console.log(isJohnOlder);
+*/
+
+/***** Variablen 04 *******/
+// Zusammenfassung Datentypen
+
+let nix;
+let ageJohn = 25;
+let firstName = "Max";
+let isJohnOlder = (10 > 5);
+
+console.log ("nix: " + typeof nix);
+console.log ("ageJohn: " + typeof ageJohn);
+console.log ("firstName: " + typeof firstName);
+console.log ("isJohnOlder: " + typeof isJohnOlder);
