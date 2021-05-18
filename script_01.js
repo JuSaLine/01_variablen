5d54466d0765c3cb06251a5363a2adef3991ce69
Peter <schmidt.multimedia@gmail.com>
2021-05-18 12:03:13 +0200

    Deklaration + Wertzuweisung II

--- script_01.js
+++ script_01.js
@@ -11,11 +11,17 @@
 //console.log(firstName);
 
 // Deklaration + Wertzuweisung I 
-/**/
+/*
 let firstName; // Deklaration (Definition)
 firstName = "Max"; // Wertzuweisung
 console.log(firstName); // Ausgabe
 let familyName = "Mütze"; // Deklaration + Wertzuweisung
 console.log(familyName); // Ausgabe
 console.log(firstName + " " + familyName);
+*/
 
+// Deklaration + Wertzuweisung II
+let firstName,familyName;
+firstName = prompt("Bitte Vornamen eingeben:");
+familyName = prompt("Bitte Namen einegeben:");
+console.log("Hallo, " + firstName + " " + familyName + "!");
